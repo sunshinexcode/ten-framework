ec298500b941593be4c132d75c60e29b48ab00cc
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-09-13 17:14:32 +0800

    chore: update version headers and manifest files across multiple languages and extensions (#1494)

--- core/include_internal/ten_runtime/common/version.h
+++ core/include_internal/ten_runtime/common/version.h
@@ -6,4 +6,4 @@
 //
 #pragma once
 
-#define TEN_RUNTIME_VERSION "0.11.7"
+#define TEN_RUNTIME_VERSION "0.11.8"
--- core/src/ten_manager/src/version.rs
+++ core/src/ten_manager/src/version.rs
@@ -1,1 +1,1 @@
-pub const VERSION: &str = "0.11.7";
+pub const VERSION: &str = "0.11.8";
